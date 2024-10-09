dee68f88e3ca163c3e51b91bded846a280d9790c
Opilane TTHK <opilane@tthk.ee>
2024-10-09 13:21:50 +0300

    Uuendus

--- javascript.js
+++
@@ -1,4 +0,0 @@
-// Funktsioon, mis näitab teadet, kui kasutaja klõpsab registreerimise nuppu
-document.querySelector("button").addEventListener("click", function() {
-    alert("Tere tulemast! Ootame teid meie platvormil.");
-});
