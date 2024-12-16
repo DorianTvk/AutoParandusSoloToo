134f392eea8710f89f8e70e21e6ce1447fa268f0
DorianTvk <127193008+DorianTvk@users.noreply.github.com>
2024-12-17 00:32:58 +0200

    Koodi korrastamine
    Korrastasin koodi natukene, proovisin muuta veebilehte ilusamaks.

--- booking.php
+++ booking.php
@@ -1,19 +1,17 @@
 <?php
-// Andmebaasi ühenduse seaded
+
 $servername = "localhost";
-$username = "root"; // Muuda oma kasutajanime
-$password = ""; // Muuda oma parooli
+$username = "root";
+$password = "";
 $dbname = "auto_hooldus";
 
-// Loome ühenduse
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Kontrollime ühendust
+
 if ($conn->connect_error) {
     die("Ühenduse loomine ebaõnnestus: " . $conn->connect_error);
 }
 
-// Kontrollime, kas vorm on saadetud
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $email = $_POST['email'];
@@ -21,7 +19,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $time = $_POST['time'];
     $service = $_POST['service'];
 
-    // Sisestame broneeringu andmebaasi
+
     $sql = "INSERT INTO bookings (username, email, date, time, service) VALUES ('$username', '$email', '$date', '$time', '$service')";
 
     if ($conn->query($sql) === TRUE) {
@@ -31,6 +29,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 
-// Sulgeme ühenduse
+
 $conn->close();
 ?>
